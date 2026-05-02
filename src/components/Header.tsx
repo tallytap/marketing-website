1d448d998921d5c2d18738eb0677a4e62e1e522b
José Antonio González Rodríguez <jagonzalr@users.noreply.github.com>
2026-05-02 12:39:43 +0100

    Remove pricing from header (#4)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,12 +20,6 @@ export default function Header() {
             >
               Features
             </Link>
-            <Link
-              to="/#pricing"
-              className="text-tt-dark hover:text-tt-green transition-colors font-medium"
-            >
-              Pricing
-            </Link>
             <a
               href="https://apps.apple.com/app/tallytap"
               target="_blank"
